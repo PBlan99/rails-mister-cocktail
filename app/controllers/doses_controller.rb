8792260a97bdc8784814421391b4c6ca27741bc2
Nick Blanco <nickblanco@y7mail.com>
2018-11-19 10:53:05 -0300

    committing latest updates

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,13 +7,14 @@ class DosesController < ApplicationController
   end
 
   def create
+    raise # this allows us to see what the dose form sends in the params when submitted
     @dose = Dose.new(dose_params) # should replace with .new(restaurant_params) to use the sanitised params, instead of the dirty one
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail # here we are specifying the restaurant_id, this is equivalent to writing @review.restaurant_id = @restaurant.id
     # @dose.ingredient_id = ingredient_id_params[:ingredient_id].to_i
     return redirect_to cocktail_path(@cocktail) if @dose.save
     @all_ingredients = Ingredient.all
-    render :new  # this goes to the new.html.erb in the doses folder, which requries @all_ingredients
+    render :new # this goes to the new.html.erb in the doses folder, which requires @all_ingredients
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
+  # get 'doses/new'
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # resources :restaurants, except: [ :edit, :update, :destroy ] do
   #   resources :reviews, only: [ :new, :create ]
   # end
 
-  resources :cocktails, except: [ :edit, :update, :destroy ] do
+  root to: 'cocktails#index'  # we make the cocktails index the root, such that http://localhost:3000 takes us to the cocktails index
+
+  resources :cocktails, except: [:index, :edit, :update, :destroy ] do
     resources :doses, only: [ :new, :create ]
   end
 
